b12d669af76eb53816c6113495759e855ab46eaf
raadmahmoud <raadmahmoud@kromek.com>
2023-08-17 09:17:41 +0100

    data_processing.py full batch generation implemented, unit tests to follow.
    First skeleton of train.py added.

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,48 +1,95 @@
 import io
 from collections import Counter
+from collections.abc import Iterable
 from dataclasses import dataclass
+from functools import partial
+from typing import Callable, List
 from os import PathLike
 
 import torch
+from torch.nn.utils.rnn import pad_sequence
 from torchtext.data.utils import get_tokenizer
-from torchtext.vocab import Vocab
+from torchtext.vocab import Vocab, build_vocab_from_iterator
 
 
 def build_vocab(filepath, tokenizer):
+    """
+    Function for counting instances of tokenized words in a language dataset
+    Args:
+        filepath: Path to file containing language examples
+        tokenizer: A tokenization pipeline such as a spacy pipeline.
+
+    Returns:
+        A torchtext.vocab.Vocab object which numericalises a language case.
+    """
     counter = Counter()
     with io.open(filepath, encoding="utf8") as f:
         for string_ in f:
             counter.update(tokenizer(string_))
-    return Vocab(counter, specials=['<unk>', '<pad>', '<bos>', '<eos>'])
+    vocab = Vocab(counter, specials=['<unk>', '<pad>', '<bos>', '<eos>'], specials_first=True)
+    vocab.set_default_index = 0
+    return vocab
+
+
+def yield_tokens(data_iterator: Iterable, tokenizer: Callable) -> List[str]:
+    for data_sample in data_iterator:
+        yield tokenizer(data_sample)
 
 
 @dataclass
 class DataProcessor:
-    language_a: str  # The language of choice we wish to translate FROM, e.g. "en"
-    language_b: str  # The language we wish to translate TO, e.g. "es"
+    """
+    Defines a processor object that stores salient information about the language we translate from (language a) and
+    the language we translate to (language b).
+    """
+    language_a: str  # The language of choice we wish to translate FROM, as a spacy pipeline e.g. "en_core_web_sm"
+    language_b: str  # The language we wish to translate TO, , as a spacy pipeline e.g. "es_core_news_sm"
     train_file_language_a: str  # A file consisting of a single sentence per line in language a
     train_file_language_b: str  # A file consisting of the same sentences as "train_file_language_a", but in language b
 
     def __post_init__(self):
+        partial_build_vocab_from_iterator = partial(build_vocab_from_iterator, min_freq=1,
+                                                    specials=['<unk>', '<pad>', '<bos>', '<eos>'], special_first=True)
+
         self.tokenizer_a = get_tokenizer('spacy', language=self.language_a)
         self.tokenizer_b = get_tokenizer('spacy', language=self.language_b)
-        self.vocab_a = build_vocab(self.train_file_language_a, self.tokenizer_a)
-        self.vocab_b = build_vocab(self.train_file_language_b, self.tokenizer_b)
+
+        with iter(io.open(self.train_file_language_a, 'r')) as train_iter_a:
+            self.vocab_a = partial_build_vocab_from_iterator(yield_tokens(train_iter_a, tokenizer=self.tokenizer_a))
+        self.vocab_a.set_default_index(0)
+        with iter(io.open(self.train_file_language_b, 'r')) as train_iter_b:
+            self.vocab_b = partial_build_vocab_from_iterator(yield_tokens(train_iter_b, tokenizer=self.tokenizer_b))
+        self.vocab_b.set_default_index
 
         self.pad_id = self.vocab_a['<pad>']
+        self.bos_id = self.vocab_a['<bos>']
+        self.eos_id = self.vocab_a['<eos>']
 
     def data_process(self, target_file_language_a: str | PathLike, target_file_language_b: str | PathLike):
         raw_a_iter = iter(io.open(target_file_language_a, encoding="utf8"))
         raw_b_iter = iter(io.open(target_file_language_b, encoding="utf8"))
-        data = []
         for (raw_a, raw_b) in zip(raw_a_iter, raw_b_iter):
-            tensor_a_ = torch.tensor([self.vocab_a[token] for token in self.tokenizer_a(raw_a)], dtype=torch.long)
-            tensor_b_ = torch.tensor([self.vocab_b[token] for token in self.tokenizer_b(raw_b)], dtype=torch.long)
-            data.append((tensor_a_, tensor_b_))
-        return data
+            yield(raw_a, raw_b)
 
-def produce_batch(tensor_batch):
-    en_batch, es_batch = [], []
-    for (en_item, es_item) in tensor_data:
-        en_batch.append(torch.)
-
+    def generate_batch(self, batch, ):
+        a_batch, b_batch = [], []
+        for (a_item, b_item) in batch:
+            a_item = self.tokenizer_a(a_item.rstrip("\n"))
+            b_item = self.tokenizer_b(b_item.rstrip("\n"))
+            a_item = self.vocab_a(a_item)
+            b_item = self.vocab_b(b_item))
+            a_batch.append(torch.cat(
+                [torch.tensor([self.bos_id]),
+                 torch.tensor(a_item),
+                 torch.tensor([self.eos_id])],
+                dim=0
+            ))
+            b_batch.append(torch.cat(
+                [torch.tensor([self.bos_id]),
+                 torch.tensor(b_item),
+                 torch.tensor([self.eos_id])],
+                dim=0
+            ))
+        a_batch = pad_sequence(a_batch, padding_value=self.pad_id)
+        b_batch = pad_sequence(b_batch, padding_value=self.pad_id)
+        return a_batch, b_batch
---
+++ train.py
@@ -0,0 +1,31 @@
+import yaml
+from torch.utils.data import DataLoader
+
+from src.data_processing import DataProcessor
+
+# Open our general config file.
+with open('config.yml', 'r') as configfile:
+    config = yaml.load(configfile, Loader=yaml.SafeLoader)
+
+# Instantiate a DataProcessor object for the languages we have specified.
+data_processor = DataProcessor(
+    language_a=config['language_a'],
+    language_b=config['language_b'],
+    train_file_language_a=config['language_a_train_file'],
+    train_file_language_b=config['language_b_train_file']
+)
+
+#Prepare a training dataset using our DataProcessor instance
+train_iter = data_processor.data_process(
+    target_file_language_a=config['language_a_train_file'],
+    target_file_language_b=config['language_b_train_file']
+)
+
+
+# Create a training data iterator to be used for the training loop
+train_dataloader = DataLoader(
+    list(train_iter),
+    batch_size=config['training']['batch_size'],
+    shuffle=True,
+    collate_fn=data_processor.generate_batch
+)
