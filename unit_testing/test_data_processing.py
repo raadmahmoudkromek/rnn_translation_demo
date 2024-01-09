a62e9ab1b1f8d5d4021e4a81ab208fee6da28fd2
raadmahmoud <raadmahmoud@kromek.com>
2024-01-09 18:05:00 +0000

    tidied test_data_processing.py

--- unit_testing/test_data_processing.py
+++ unit_testing/test_data_processing.py
@@ -1,6 +1,9 @@
+from unittest.mock import Mock
+
 import pytest
-from unittest.mock import Mock, mock_open, patch, MagicMock
+
 from src.data_processing import DataProcessor
+
 
 @pytest.fixture
 def sample_data(tmpdir):
@@ -10,11 +13,13 @@ def sample_data(tmpdir):
     file_b.write("Translated sentence 1.\nAnother translated sentence here.\n")
     return str(file_a), str(file_b)
 
+
 @pytest.fixture
 def mock_tokenizer():
     tokenizer = Mock()
     tokenizer.side_effect = lambda x: x.split()
     return tokenizer
+
 
 def test_data_processor_init(sample_data):
     dp = DataProcessor("en_core_web_sm", "es_core_news_sm", *sample_data)
@@ -24,11 +29,13 @@ def test_data_processor_init(sample_data):
     assert dp.bos_id == dp.vocab_a['<bos>']
     assert dp.eos_id == dp.vocab_a['<eos>']
 
+
 def test_generate_batch(sample_data):
     dp = DataProcessor("en_core_web_sm", "es_core_news_sm", *sample_data)
     mock_batch = [("This is a sentence.", "Translated sentence.")]
     a_batch, b_batch = dp.collation(mock_batch)
     assert a_batch.shape[1] == b_batch.shape[1]  # Both should have the same sequence length
+
 
 def test_generate_batch_padding(sample_data):
     dp = DataProcessor("en_core_web_sm", "es_core_news_sm", *sample_data)
