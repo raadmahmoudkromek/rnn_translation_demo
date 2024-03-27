ac6796dca6af090ed63a7175ef80224fc22b1bcc
raadmahmoud <raadmahmoud@kromek.com>
2024-03-27 14:03:44 +0000

    train.py completed with example translation call at end and corrected epoch losses

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import yaml
 from torch.utils.benchmark import timer
@@ -7,6 +8,7 @@ from src.data_processing import DataProcessor, pairwise_sentence_iterator
 from src.device import DEVICE
 from src.evaluate import evaluate_model
 from src.model import Sequence2SequenceTransformer, training_epoch
+from src.predict import translate
 
 torch.manual_seed(0)
 
@@ -82,8 +84,12 @@ for epoch in range(1, config['training']['num_epochs'] + 1):
                               loss_fn=loss_func,
                               create_mask=data_processor.create_mask)
     end_time = timer()
-    print(f"Epoch: {epoch}, Train loss: {train_loss:.3f}, "f"Epoch time = {(end_time - start_time):.3f}s")
+    print(f"Epoch: {epoch},"
+          f" Train loss: {np.average(train_loss):.3f},"
+          f" Val loss: {np.average(val_loss):.3f},"
+          f" "f"Epoch time = {(end_time - start_time)}s")
 
 
+# Example usage of the resultant model:
 
-evaluate_model(transformer, test_dataloader, loss_func=loss_func)
+print(translate(model=transformer, data_processor=data_processor, src_sentence="Get up."))
